629f8896d505fca151a558483c06d55d479b961b
Mateusz Ż <zochowski.mateusz@outlook.com>
2022-07-26 13:08:25 +0200

    style: update colors and UI width

--- src/App.tsx
+++ src/App.tsx
@@ -43,8 +43,8 @@ function App() {
   }
 
   const timersListClasses = 'my-2';
-  const inputWrapperClasses = 'w-full my-3';
-  const inputErrorClasses = 'text-sm text-red-500 -mt-0.5';
+  const inputWrapperClasses = 'w-72 mx-auto my-3';
+  const inputErrorClasses = 'text-sm text-nordred';
 
   const removeTimer = (id: number) => {
     console.log('hehehe remove timer', id);
@@ -55,90 +55,92 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen text-center bg-slate-100">
-      <header className="py-2">
-        <p>countdown</p>
-      </header>
+    <div className="flex flex-col justify-center items-center h-screen text-center bg-nordlightgray">
+      <div className="w-80">
+        <header className="py-2">
+          <p>countdown</p>
+        </header>
 
-      <div>
-        {timers?.length == 0 &&
-         <div className={timersListClasses}>No timers created :(</div>}
+        <div>
+          {timers?.length == 0 &&
+           <div className={timersListClasses}>No timers created :(</div>}
 
-        {timers?.length > 0 && <ul className={timersListClasses}>
-          {timers.map((timer: ITimer, key: number) => (
-            <Timer
-              key={key}
-              title={timer.title}
-              date={timer.date}
-              onClick={() => { removeTimer(key) }}
-            />
-          ))}
-        </ul>}
+          {timers?.length > 0 && <ul className={timersListClasses}>
+            {timers.map((timer: ITimer, key: number) => (
+              <Timer
+                key={key}
+                title={timer.title}
+                date={timer.date}
+                onClick={() => { removeTimer(key) }}
+              />
+            ))}
+          </ul>}
 
-        <Formik
-          initialValues={{
-            title: '',
-            date: '',
-          }}
-          onSubmit={(
-            values: ITimer,
-            { resetForm }: FormikHelpers<ITimer>
-          ) => {
-            const newTimer: ITimer = values;
-            setTimers([...timers, newTimer]);
-            resetForm();
-          }}
-          validate={validateForm}
-        >
-          {({ errors, touched }) => (
-            <Form className="my-8">
-              <div className={inputWrapperClasses}>
-                <label
-                  htmlFor="title"
-                >
-                  Title
-                </label>
-                <Field
-                  id="title"
-                  name="title"
-                  placeholder="Title"
-                />
-                {errors.title && touched.title &&
-                 <div className={inputErrorClasses}>{errors.title}</div>}
-              </div>
-
-              <div className={inputWrapperClasses}>
-                <label
-                  htmlFor="date"
-                >
-                  Date
-                </label>
-                <Field
-                  id="date"
-                  name="date"
-                  placeholder="date"
-                  type="datetime-local"
-                />
-                {errors.date && touched.date &&
-                 <div className={inputErrorClasses}>{errors.date}</div>}
-              </div>
-
-              <button type="submit">Add timer</button>
-            </Form>
-          )}
-        </Formik>
-      </div>
-      <footer className="text-xs text-gray-700">
-        <p>
-          Made by
-          <a
-            href="#"
+          <Formik
+            initialValues={{
+              title: '',
+              date: '',
+            }}
+            onSubmit={(
+              values: ITimer,
+              { resetForm }: FormikHelpers<ITimer>
+            ) => {
+              const newTimer: ITimer = values;
+              setTimers([...timers, newTimer]);
+              resetForm();
+            }}
+            validate={validateForm}
           >
-            mateossh
-          </a>
-        </p>
-        <a href="https://github.com/mateossh/countdown2">Source code</a>
-      </footer>
+            {({ errors, touched }) => (
+              <Form className="my-8">
+                <div className={inputWrapperClasses}>
+                  <label
+                    htmlFor="title"
+                  >
+                    Title
+                  </label>
+                  <Field
+                    id="title"
+                    name="title"
+                    placeholder="Title"
+                  />
+                  {errors.title && touched.title &&
+                   <div className={inputErrorClasses}>{errors.title}</div>}
+                </div>
+
+                <div className={inputWrapperClasses}>
+                  <label
+                    htmlFor="date"
+                  >
+                    Date
+                  </label>
+                  <Field
+                    id="date"
+                    name="date"
+                    placeholder="date"
+                    type="datetime-local"
+                  />
+                  {errors.date && touched.date &&
+                   <div className={inputErrorClasses}>{errors.date}</div>}
+                </div>
+
+                <button type="submit">Add timer</button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+        <footer className="text-xs text-gray-700">
+          <p>
+            Made by
+            <a
+              href="#"
+            >
+              mateossh
+            </a>
+          </p>
+          <a href="https://github.com/mateossh/countdown2">Source code</a>
+        </footer>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
           '500': '#5E81AC',
         },
         'grey': '#6e778a', // so this doesn't override default 'gray' heheh
+        'norddarkgray': '#2e3440',
+        'nordlightgray': '#eceff4',
+        'nordred': '#bf616a',
       },
     },
   },
