eeda95ff03b1d045f504eb6c5b753beb1365cb67
Mateusz Ż <zochowski.mateusz@outlook.com>
2022-07-26 12:55:02 +0200

    feat: use custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
       fontSize: {
         'testsize': '.825rem',
       },
+      colors: {
+        'primary': {
+          '400': '#81A1C1',
+          '500': '#5E81AC',
+        },
+        'grey': '#6e778a', // so this doesn't override default 'gray' heheh
+      },
     },
   },
   plugins: [],
